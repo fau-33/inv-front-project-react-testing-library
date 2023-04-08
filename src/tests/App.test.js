1f339cb956560aa291513eb6106b87a7ba4bbdbb
Flávio Félix <fau_felix30@hotmail.com>
2023-04-08 01:09:17 -0300

    Feito o req 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,17 +1,16 @@
 import { screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
-describe('Tests for component App.js', () => {
+describe('Tests for App.js', () => {
   it('contains a fixed set of navigation links', () => {
     renderWithRouter(<App />);
 
     const homeLink = screen.getByRole('link', { name: 'Home' });
     const aboutLink = screen.getByRole('link', { name: 'About' });
-    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
 
     expect(homeLink).toBeInTheDocument();
     expect(aboutLink).toBeInTheDocument();
@@ -37,22 +36,19 @@ describe('Tests for component App.js', () => {
   it('when clicking the Favorite Pokémons link, redirect to URL /favorites', () => {
     const { history } = renderWithRouter(<App />);
 
-    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
     userEvent.click(favoritesLink);
     expect(history.location.pathname).toBe('/favorites');
   });
 
   it('redirected to Not Found page when entering unknown URL', () => {
     const { history } = renderWithRouter(<App />);
-    const TEXT_NOT_FOUND = 'Page requested not found';
+    const TEXT_NOT_FOUND = /Page requested not found/i;
 
-    act(() => {
-      history.push('/notFoundUrl');
-    });
-    const notFoundHeading = screen.getByRole('heading', { name: TEXT_NOT_FOUND, level: 2 });
-    const notFoundImage = screen.getByAltText('Pikachu crying because the page requested was not found');
+    history.push('/notFoundUrl');
+    const notFound = screen.getByRole('heading', { name: TEXT_NOT_FOUND,
+      level: 2 });
 
-    expect(notFoundHeading).toBeInTheDocument();
-    expect(notFoundImage).toBeInTheDocument();
+    expect(notFound).toBeInTheDocument();
   });
 });
---
+++ src/tests/FavoritePokemon.test.js
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemon';
+import renderWithRouter from './renderWithRouter';
+
+describe('Tests for page FavoritePokemons', () => {
+  it('the message No favorite pokemon found is displayed on the screen', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
