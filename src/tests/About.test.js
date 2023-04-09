72a629bc67424c30899b57a1a37888a424c796d6
Flávio Félix <fau_felix30@hotmail.com>
2023-04-08 23:04:19 -0300

    Reenviando o req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,15 +1,18 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import About from '../pages/About';
 import renderWithRouter from './renderWithRouter';
 
-describe('Tests for page About', () => {
-  it('Testing if the page has h2 with About Pokedex text', () => {
+describe('Tests for page About.js', () => {
+  it('must have an h2 header with text About Pokédex', () => {
     renderWithRouter(<About />);
-    const aboutPokedex = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+
+    const aboutPokedex = screen.getByRole('heading', { name: /About Pokédex/i,
+      level: 2 });
     expect(aboutPokedex).toBeInTheDocument();
   });
-  it('Must have an img with src specific', () => {
+
+  it('must have an img with src specific', () => {
     render(<About />);
     const SRC_IMAGE_POKEDEX = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
 
